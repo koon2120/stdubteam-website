abe3269d6d0fa07c0596e027bd29300d9321b9b7
Koon <gameskoon101020@gmail.com>
2024-06-05 19:27:45 +0700

    version 1.1.3
    fix favicon not show

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
-  modules: ["@nuxt/content"]
+  modules: ["@nuxt/content"],
+  app: {
+      head: {
+        link: [
+          { rel: "icon", type: "image/png", href: "https://firebasestorage.googleapis.com/v0/b/stdubteam-website.appspot.com/o/webassets-webp%2Ffavicon.png?alt=media" }
+        ]
+    }
+  }
 })
